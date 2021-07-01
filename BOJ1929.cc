8593fc9e37894b20ab9f9e0bcdc0ccfc6fef52a9
alsdream <AlsdnWorks@gmail.com>
2021-07-01 15:15:25 +0900

    July1

---
+++ BOJ1929.cc
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include<cmath>
+using namespace std;
+int testa; int testb;
+
+void ptest(int a,int b){
+	bool* list = new bool[b + 1];
+	for (int i = 2; i <= b; i++)
+		list[i] = true;
+	for (int i = 2; i * i <= b; i++){
+		if (list[i])
+			for (int j = i * i; j <= b; j += i)
+				list[j] = false;
+	}
+	for (int i = a; i <= b; i++){
+		if (list[i] == true)
+			printf("%d\n", i);
+	}
+	free(list);
+}
+int main() {
+	scanf("%d %d", &testa,&testb);
+	ptest(testa,testb);
+	return 0;
+}
