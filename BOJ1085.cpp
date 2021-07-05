efb0d781758b81681986eef2ec68209b9c44b56c
alsdream <AlsdnWorks@gmail.com>
2021-07-05 23:44:55 +0900

    July5

---
+++ BOJ1085.cpp
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+int x; int y; int w; int h;
+int main() {
+	int tempX; int tempY;
+	scanf("%d %d %d %d", &x, &y, &w, &h);
+	if (x > (w / 2))  tempX = w - x;
+	else  tempX = x;
+	if (y > (h / 2))  tempY = h - y;
+	else  tempY = y;
+	if (tempX > tempY)  printf("%d", tempY);
+	else  printf("%d", tempX);
+	return 0;
+}
