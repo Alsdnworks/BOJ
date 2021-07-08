96e9af7e568f666ee62a76674bfda061b68bb2c8
alsdream <AlsdnWorks@gmail.com>
2021-07-08 23:54:40 +0900

    July8

---
+++ BOJ1427.cc
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <vector>
+#include<algorithm>
+#include<functional>
+using namespace std;
+int main() {
+	vector <int> buffer;buffer.reserve(100);int m_set,set;int i = 0;
+	cin >> set;
+	while (set > 1) {
+		buffer.push_back(set % 10);
+		set /= 10;
+	}
+	sort(buffer.begin(), buffer.end(),greater<int>());
+	for (i = 0; i < buffer.size(); i++)
+		cout << buffer[i];
+	return 0;
+}
