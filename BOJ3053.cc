77716ceb4d53c23374d08fb4fd9788e654082963
alsdream <AlsdnWorks@gmail.com>
2021-07-17 19:05:25 +0900

    July17

---
+++ BOJ3053.cc
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    int r;
+    cin >> r;
+    cout << fixed;cout.precision(6);
+    cout<< r * r * 3.14159265359 << endl << 2 * (r * r);
+    return 0;
+}
---
+++ BOJ7568.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include<vector>
+#include<tuple>
+using namespace std;
+vector<tuple<int, int, int>> v;
+int main() {
+	v.reserve(99); int r, i;
+	cin >> r;
+	for (i = 0; i < r; i++) {
+		int x, y;
+		cin >> y >> x;
+		v.push_back((tuple<int, int, int>(x, y, 1)));
+	}
+	for (i = 0; i < r; i++) 
+		for (int j = 0; j < r; j++) 
+			if (get<0>(v[j]) < get<0>(v[i]) && get<1>(v[j]) < get<1>(v[i])) 
+				get<2>(v[j])++;
+	for (i = 0; i < r; i++) 
+		cout << get<2>(v[i]) << endl;
+	return 0;
+}
