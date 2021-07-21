854f09580808e09e0749e022ab2f77feab6a63b0
alsdream <AlsdnWorks@gmail.com>
2021-07-21 19:54:10 +0900

    July21-ing

---
+++ BOJ2108푸는중.cpp
@@ -0,0 +1,46 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include<vector>
+#include<utility>
+#include <algorithm>
+#include<cmath>
+using namespace std;
+vector<pair<int, int>>v;
+int rep; int range; int x;
+int sum = 0; int j = 0; int i = 0;
+bool sortB(pair<int, int>a, pair<int, int>b) {
+	if (a.second != b.second)	return(a.second > b.second);
+	else 	return(a.first < b.first);
+}
+int main() {
+	v.reserve(500003);
+	scanf("%d", &rep);
+	for (i = 0; i < rep; i++) {
+		scanf("%d", &x);
+		v.push_back((pair<int, int>(x, 0)));
+		sum += x;
+	}
+	sort(v.begin(), v.end());
+	range = (v.back().first) - (v.front().first);
+	for (i = 0; i < rep; i++) {
+		if (i == 0)
+			v[i].second = 0;
+		else if (v[i].first != v[i - 1].first) {
+			j = 0;
+			v[i].second = 0;
+		}
+		else {
+			j++;
+			v[i].second = j;
+		}
+	}
+	printf("%.0f\n", double(sum / rep));
+	printf("%d\n", v[round(rep / 2)].first);
+	sort(v.begin(), v.end(), sortB);
+	if (v.size() == 1 || v[0].second > v[1].second)
+		printf("%d\n", v[0].first);
+	else
+		printf("%d\n", v[1].first);
+	printf("%d\n", range);
+	return 0;
+}
---
+++ BOJ9663푸는중.cpp
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+using namespace std;
+int n; int out=0;
+int pan[15];
+//rep as row i as columm
+int backtrack(int piv, int rep) {
+	bool flag=false;
+	if (piv == n)
+		return(out);
+	if (rep == n) {
+		for (int i = 1; i <= n; i++)
+			if (pan[i] != 0)
+				out++;
+		backtrack(piv + 1, 1);
+	}
+	for (int i = 1; i < n; i++) {
+			if (rep == 1) {
+				pan[i] = piv;
+				break;
+			}
+			else if (pan[j] != pan[i] && abs(pan[i] - pan[j]) != abs(i-j)) {
+				pan[rep] = i;
+				backtrack(piv, rep + 1);
+			}
+		}
+		backtrack(piv, rep + 1);
+	}
+}
+
+		//if (pan[j] == i || pan[j] - j == i || pan[j] + j == i)
+int main() {
+	scanf("%d", &n);
+	backtrack(1,1);
+	printf("%d", out);
+	return 0;
+}
