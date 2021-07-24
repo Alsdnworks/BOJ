c3461c2f0c070a2487a00ee8f2324eb1133be03a
alsdream <AlsdnWorks@gmail.com>
2021-07-24 19:08:14 +0900

    July24

---
+++ BOJ13305.cpp
@@ -0,0 +1,22 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+pair<int, int>v[100000];
+long long m = 0; int n,i,j,d;
+int main() {
+	d = 0;
+	cin>>n;
+	for(i=0;i<n-1;i++)
+		cin>>v[i].second;
+	v[n].second = 0;
+	for (i = 0; i < n; i++)
+		cin>>v[i].first;
+	for (i = 0; i < n;) {
+		while (v[i].first<=v[j].first) {
+			d += v[j].second;j++;
+		}
+		m += (long long)d * v[i].first;
+		i = j;d = 0;
+	}
+	cout<<m;
+	return 0;
+}
