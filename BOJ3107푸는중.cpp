3842283be512672eec534f2e39faa7217a568bc6
alsdream <AlsdnWorks@gmail.com>
2021-07-30 16:25:56 +0900

    July30

---
+++ BOJ3107푸는중.cpp
@@ -0,0 +1,37 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+using namespace std;
+char ip[50]; int i=0,n=0;
+vector <char> seg;
+string long_ip;
+int main(){
+	scanf("%s\0",ip);
+	while (ip[i]!= '\0') {
+		seg.push_back(ip[i]);
+		if (ip[i+1]!=':'&&ip[i]==':'&&ip[i-1]!=':') {
+			while (seg.size() != 5)
+				seg.insert(seg.begin(), '0');
+			for (int j = 0; j < 4; j++) {
+				long_ip += seg[j];
+			}
+			seg.clear();
+			long_ip += ':';
+		}
+		i++;
+	}
+	if (ip[i + 1] != ':' && ip[i - 1] != ':') {
+		while (seg.size() != 4)
+			seg.insert(seg.begin(), '0');
+		for (int j = 0; j < 4; j++) {
+			long_ip += seg[j];
+		}
+	}
+	for (int i = 0; long_ip[i] ==':' && long_ip[i+1]==':';i++)
+		while (long_ip.size()<=39){
+			long_ip.insert(i, "0000:");
+			i+=5;
+		}
+	//long_ip에서 ::를 찾아서 39가될때까지 :0000을 추가
+	cout << long_ip;
+	return 0;
+}
---
+++ BOJ4949푸는중.cpp
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int j = 0; char in[108]; stack<char>s; {
+	j = 0;
+	scanf("%[^\n]s", &in);
+	while (in[0] != '.') {
+
+		if (in[j] == ')') {
+			if (!s.empty())
+				if (s.top() == 1) {
+					s.pop();
+				}
+
+		}
+		if (in[j] == ']') {
+			if (!s.empty())
+				if (s.top() == 2) {
+					s.pop();
+				}
+		}
+		if (in[j] == '(')
+			s.push(1);
+		if (in[j] == '[')
+			s.push(2);
+		if (s.empty())
+			printf("YES\n");
+		else
+			printf("NO\n");
+		j++;
+	}
+
+	return 0;
+}
