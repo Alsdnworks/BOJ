935eca4ccc6a2f47d209b25fbe4c23994b40e7db
alsdream <AlsdnWorks@gmail.com>
2021-08-04 21:16:27 +0900

    Aug4 BOJ3107

---
+++ BOJ3107.cpp
@@ -0,0 +1,48 @@
+﻿#include<bits/stdc++.h>
+	//3107번: IPv6
+	/*참고자료:https://blockdmask.tistory.com/338[[C++] [string 클래스, 문자열에 대해서 (총정리)[개발자 지망생]] 
+	디코딩할 ip를 입력받는다.
+	ip의 끝까지 반복하며 :(콜론) ::(더블콜론)을 만날때 함수를 사용할것이기에 첫부분에 임의로 콜론을 삽입한다.
+	콜론을 만나면 콜론다음으로 이동해 다음 콜론 또는 ip의 끝까지 세그먼트(seg)에 내용을 넣어준다 처음에 벡터를 사용했으나 간결성을 위해 스트링사용
+	세그먼트에 입력이 끝나면 네자리가 될때까지 0을 채워넣어준다. 세그먼트내용을 _ip에 넣어주고 초기화
+	더블콜론을 만나면 콜론 다음으로 이동하고 m_ip에 더블콜론의 위치를 표시하는 '.'을 넣어준다.
+	ip의 끝까지 이동하면 32자리의 수 또는 몇자리수와 '.'이 m_ip에 저장되어있다.
+	총길이 32에서 ip.size()만큼 빼주고 빼준값이있다면 그만큼의 '0'을 생성해 '.'과 대치시킨다
+	4자리마다 :을 삽입해 ipv6의 형태를 잡아 출력한다.*/
+using namespace std;
+int i = 0,n;
+string _ip,ip,seg;
+
+void pbc(int &i) {
+	for (;(ip[i] != ':') && (i < ip.size());i++) {
+		seg += ip[i];
+	}
+	while (seg.size() < 4)
+		seg.insert(seg.begin(), '0');
+	_ip += seg;
+	seg.clear();
+}
+int main() {
+	cin>>ip;
+	ip.insert(ip.begin(),':');
+	for (;i < ip.size();)
+		if (ip[i] == ':' && ip[i + 1] == ':') {
+			i += 2;
+			_ip += '.';
+			pbc(i);
+		}
+		else if (ip[i] == ':') {
+			i++;
+			pbc(i);
+		}
+	n = 32 -_ip.size();
+	for (i = 0; i <= n; i++)seg += '0';
+	if (n != 0) {
+		_ip.insert(_ip.find('.'), seg);
+		_ip.erase(_ip.find('.'), 1);
+	}
+	for (i = 4; i < _ip.size(); i += 5)
+		_ip.insert(i, ":");
+	cout << _ip;
+	return 0;
+}
--- BOJ3107푸는중.cpp
+++ BOJ3107푸는중.cpp
@@ -1,14 +1,15 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include<bits/stdc++.h>
 using namespace std;
-char ip[50]; int i=0,n=0;
+char ip[50], * loc; int i = 0, n = 0;
 vector <char> seg;
 string long_ip;
-int main(){
-	scanf("%s\0",ip);
-	while (ip[i]!= '\0') {
+bool flag = false, m_flag = true;
+int main() {
+	scanf("%s\0", ip);
+	while (ip[i] != '\0') {
 		seg.push_back(ip[i]);
-		if (ip[i+1]!=':'&&ip[i]==':'&&ip[i-1]!=':') {
+		if (ip[i + 1] != ':' && ip[i] == ':' && ip[i - 1] != ':') {
 			while (seg.size() != 5)
 				seg.insert(seg.begin(), '0');
 			for (int j = 0; j < 4; j++) {
@@ -17,21 +18,49 @@ int main(){
 			seg.clear();
 			long_ip += ':';
 		}
+		if (ip[i] == ':' && ip[i + 1] == ':') {
+			for (int j = 0; j < seg.size(); j++) {
+				long_ip += seg[j];
+			}
+			long_ip += ':';
+			seg.clear();
+			flag = true;
+			i += 2;
+			continue;
+		}
 		i++;
+
 	}
+
 	if (ip[i + 1] != ':' && ip[i - 1] != ':') {
-		while (seg.size() != 4)
+		while (seg.size() < 4)
 			seg.insert(seg.begin(), '0');
 		for (int j = 0; j < 4; j++) {
 			long_ip += seg[j];
 		}
 	}
-	for (int i = 0; long_ip[i] ==':' && long_ip[i+1]==':';i++)
-		while (long_ip.size()<=39){
-			long_ip.insert(i, "0000:");
-			i+=5;
+	if (flag == true) {
+		i = 0;
+		while (i < long_ip.size()) {
+			if (long_ip[i] == ':' && long_ip[i + 1] == ':')break;
+			i++;
 		}
-	//long_ip에서 ::를 찾아서 39가될때까지 :0000을 추가
+		while (long_ip.size() <= 39) {
+			long_ip.insert(i + 1, "0000:");
+			i += 5;
+		}
+		if (long_ip[0] = ':')
+			long_ip.erase(0, 1);
+		while (m_flag == true) {//시작부터 탐색하는 형식으로
+			m_flag = false;
+			for (int i = 0; i < long_ip.size() - 1; i++) {
+				if (long_ip[i] == ':' && long_ip[i + 1] == ':') {
+					long_ip.erase(i, 1);
+					m_flag = true;
+				}
+			}
+		}
+	}
 	cout << long_ip;
 	return 0;
 }
