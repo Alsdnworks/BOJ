b5dafc72504d96d7fa9886d565b0e1c1d899acd5
alsdream <AlsdnWorks@gmail.com>
2021-08-17 23:45:43 +0900

    temppang

--- BOJ2667푸는중.cpp
+++ BOJ2667푸는중.cpp
@@ -1,57 +1,66 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <bits/stdc++.h>
 using namespace std;
-int board[101][101], M, N, i, j, nX, nY;
+int board[101][101], M, N, i, j, nX, nY, m_lvl;
 char b_buffer[101];
-pair <int, int> nxt;
+pair<int, int> nxt;
 bool f_deadend;
 
-void flagtest(int x, int y) {
-    if (board[x][y + 1] == 1) {
-        nxt.first = x; nxt.second = y + 1;
+void flagtest(int x, int y){
+    if (board[x][y + 1] == 1){
+        nxt.first = x;
+        nxt.second = y + 1;
     }
-    else if (board[x][y - 1] == 1) {
-        nxt.first = x; nxt.second = y - 1;
+    else if (board[x][y - 1] == 1){
+        nxt.first = x;
+        nxt.second = y - 1;
     }
-    else if (board[x + 1][y] == 1) {
-        nxt.first = x + 1; nxt.second = y;
+    else if (board[x + 1][y] == 1){
+        nxt.first = x + 1;
+        nxt.second = y;
     }
-    else if (board[x - 1][y] == 1) {
-        nxt.first = x - 1; nxt.second = y;
+    else if (board[x - 1][y] == 1){
+        nxt.first = x - 1;
+        nxt.second = y;
     }
-    else f_deadend=true;
+    else
+        f_deadend = true;
 }
 
-void BST(int x, int y, int lvl) {
-    while(1){
-    if(f_deadend==true){
-        f_deadend=false;
-        if (x != 0 || y != 0) {
-        board[x][y] = lvl;
+void BST(int x, int y, int lvl){
+    while (1){
+        m_lvl = lvl;
+        if (x == N && y == M){
+            nxt.first = x;
+            nxt.second = y;
+            return;
         }
-        return;
-    }
-    if (x == N && y == M) {
-        nxt.first = x; nxt.second=y;
-        return;
-    }
-    if (x != 0 || y != 0) {
-        board[x][y] = lvl;
-    }
-    flagtest(x, y);
-    BST(nxt.first, nxt.second, ++lvl);
+        if (x != 0 || y != 0){
+            board[x][y] = lvl;
+        }
+        flagtest(x, y);
+        if (f_deadend == true){
+            f_deadend = false;
+            if (x != 0 || y != 0){
+                board[x][y] = lvl;
+            }
+            lvl--;
+            return;
+        }
+        BST(nxt.first, nxt.second, ++lvl);
     }
 }
 
-int main() {
+int main(){
     scanf("%d %d", &N, &M);
-    for (i = 0; i < N; i++) {
+    for (i = 0; i < N; i++){
         scanf("%s", &b_buffer);
-        for (j = 0; j < M; j++) {
-            board[i][j] = (int)b_buffer[j]-48;
+        for (j = 0; j < M; j++){
+            board[i][j] = (int)b_buffer[j] - 48;
         }
     }
     board[0][0] = 100;
     BST(0, 0, 1);
+    printf("%d", m_lvl);
     return 0;
 }
