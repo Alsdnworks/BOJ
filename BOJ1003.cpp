593bcd40db6104db6c8740f2465d8fd04fff5b59
Alsdnworks <alsdnworks@gmail.com>
2021-09-01 19:06:06 +0900

    DP_fibonacci(1003)

---
+++ BOJ1003.cpp
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+int iter, in, fiboBuffer[41] = { 0,1 };
+int main() {
+    scanf("%d", &iter);
+    for (int i = 0; i < iter; i++) {
+        scanf("%d", &in);
+        if (in == 0) printf("1 0\n");
+        else {
+            for (int i = 2; i <= in; i++) {
+                if (fiboBuffer[i] == 0)
+                    fiboBuffer[i] = fiboBuffer[i - 2] + fiboBuffer[i - 1];
+            }
+
+            printf("%d %d\n", fiboBuffer[in - 1], fiboBuffer[in]);
+        }
+    }
+    return 0;
+}
