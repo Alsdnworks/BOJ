cc60765d130b61a8be1c5ddafcd1697ea4034592
Alsdnworks <alsdnworks@gmail.com>
2022-02-03 19:10:34 +0900

    Fab3

---
+++ BOJ1149.cc
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+int buffer[1001][3];
+int in, cost;
+int main() {
+    scanf("%d", &in);
+    for (int i = 0; i < in; i++) {
+        scanf("%d %d %d", &buffer[i][0], &buffer[i][1], &buffer[i][2]);   
+    }
+    for (int i = 1; i <= in; i++) {
+        buffer[i][0] += min(buffer[i - 1][1], buffer[i - 1][2]);
+        buffer[i][1] += min(buffer[i - 1][0], buffer[i - 1][2]);
+        buffer[i][2] += min(buffer[i - 1][0], buffer[i - 1][1]);
+    }
+    printf("%d", min(buffer[in][2],min(buffer[in][0], buffer[in][1])));
+    return 0;
+}
